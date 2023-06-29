3065c6f0921b4a169b4f0e192278b502fd155326
rachit <curiousrachit.26@gmail.com>
2023-06-29 22:19:37 +0530

    completed

--- Random Password Generator/random.js
+++ Random Password Generator/random.js
@@ -1,7 +1,30 @@
-const SelctedInput=document.getElementsByClassName("Password");
-const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase="abcdefghijklmnopqrstuvwxyz";
-const number="1234567890";
-const symboles="!@#$%^&*()_+?:{}[]~|()";
+const PasswordInput = document.getElementById( "input-password");
+const UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LowerCase = "abcdefghijklmnopqrstuvwxyz";
+const Number = "1234567890";
+const Symboles = "!@#$%^&*()_+?:{}[]~|()";
+const lenght = 12;
 
 
+const allChars = UpperCase + LowerCase + Number + Symboles;
+
+function generated (){
+
+
+    let Password = "";
+    Password += UpperCase[Math.floor(Math.random()* UpperCase.length)];
+    Password += LowerCase[Math.floor(Math.random()* LowerCase.length)];
+    Password += Number[Math.floor(Math.random()* Number.length)];
+    Password += Symboles[Math.floor(Math.random()* Symboles.length)];
+    while (lenght > Password.length) {
+
+
+        Password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    PasswordInput.value = Password;
+}
+const CopyItem=document.getElementById("copy-item");
+CopyItem.addEventListener("click",copy=()=>{
+    PasswordInput.select();
+document.execCommand("copy");
+})
