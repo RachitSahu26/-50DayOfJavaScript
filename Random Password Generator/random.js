9da1386d63b5bd71aa4e9853f6f204852cb0cc13
rachit <curiousrachit.26@gmail.com>
2023-06-29 18:46:05 +0530

    Created a Random Generator Password

---
+++ Random Password Generator/random.js
@@ -0,0 +1,7 @@
+const SelctedInput=document.getElementsByClassName("Password");
+const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase="abcdefghijklmnopqrstuvwxyz";
+const number="1234567890";
+const symboles="!@#$%^&*()_+?:{}[]~|()";
+
+
