9993121acfa05b9ee6e172d2f3f4575589f02f45
rachit <curiousrachit.26@gmail.com>
2023-07-13 11:24:46 +0530

    completed infinite more add item

--- Gallery image search/index.js
+++ Gallery image search/index.js
@@ -1,49 +1,29 @@
-const input = document.getElementById("input");
-const searchForm = document.getElementById("search-form");
-const showingResult = document.getElementById("showing-result");
-const searchBtn= document.getElementById("search-btn");
+const searchClick = document.getElementById("search-btn");
 
-const accessKey = "7IlCKWs7Wf9DKbGFpQ2zbftDTRzzfbQydwSECbfear4";
-let keyword = "";
+
+const showingResult = document.querySelector('imag-box1');
+
+let keyword = '';
 let page = 1;
-async function searchImage() {
+async function getImage() {
     keyword = input.value;
-    const url = `https://api.unsplash.com/search/photos?page=
-    ${page}&query=${keyword}&client-id=${accessKey}`;
-    const response = await fetch(url);
+    const url = `https://api.unsplash.com/search/collections?page=${page}&query=${keyword}office&client_id=7IlCKWs7Wf9DKbGFpQ2zbftDTRzzfbQydwSECbfear4`;
+
+    const response = await fetch(url)
     const data = await response.json();
-    // const results = data.results;
-
-
-console.log(data);
+  
+    const results = data.results;
+    results.map((result) => {
+        let image = document.createElement('img')
+        image.src = result.urls.small;
+        showingResult.appendChild(image);
+    })
 
 
 }
-
-
-searchForm.addEventListener("submit", (e) => {
+searchClick.addEventListener("click", (e) => {
     e.preventDefault();
     page = 1;
 
-     searchImage();
+    getImage()
 })
-
-
-    // results.map((result) => {
-    //     const image = document.createElement('img');
-    //     image.src = results.urls.small;
-    //     const achorLink = document.createElement('a');
-
-    //     achorLink.href = results.links.html;
-
-    //     achorLink.target = "_blank";
-    //     image.appendChild(achorLink);
-    //     showingResult.appendChild(image);
-
-
-    // })
-
-
-
-
-
