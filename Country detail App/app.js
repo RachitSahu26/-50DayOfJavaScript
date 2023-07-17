842b3e3e048c959ceee16821ef89eaee045bb872
rachit <curiousrachit.26@gmail.com>
2023-07-17 11:10:52 +0530

    again  i made responsive

--- Country detail App/app.js
+++ Country detail App/app.js
@@ -7,9 +7,9 @@ const showResult = document.getElementById("show-result");
 let Capital = document.getElementById(" Capital");
 let Continent = document.getElementById("Continent");
 let Population = document.getElementById("Population");
-let CommanLanguage= document.getElementById("Comman-Language");
+let CommanLanguage = document.getElementById("Comman-Language");
 let Curreny = document.getElementById("Curreny");
-let flags=document.getElementById("flags");
+let flags = document.getElementById("flags");
 
 
 const inputBox = document.getElementById("input-box");
@@ -19,24 +19,31 @@ btn.addEventListener("click", () => {
 
     let country = inputBox.value;
     let url = `https://restcountries.com/v3.1/name/${country}`;
-   
 
-    fetch( url )
+
+    fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            showResult.style.display='block';
-            countryData=data[0]
-console.log(countryData)
-            flags.src= countryData.flags.svg;
-            Continent.innerHTML= countryData.continents
-            Population.innerHTML= countryData.population
-            Curreny.innerHTML=countryData.currencies[Object.keys(countryData.currencies)].name 
-            Capital.innerHTML= countryData.capital
 
-            CommanLanguage.innerHTML=Object.values(countryData.languages).toString().split(',').join(',');
+
+            console.log(data);
+            countryData = data[0]
+
+            showResult.style.display = 'block';
+
+            console.log(countryData)
+
+            flags.src = countryData.flags.svg;
+            Continent.innerHTML = countryData.continents
+            Population.innerHTML = countryData.population
+            Curreny.innerHTML = countryData.currencies[Object.keys(countryData.currencies)].name
+            Capital.innerHTML = countryData.capital
+
+            CommanLanguage.innerHTML = Object.values(countryData.languages).toString().split(',').join(',');
+
 
         })
+        inputBox.value ='';
 
 
 })
