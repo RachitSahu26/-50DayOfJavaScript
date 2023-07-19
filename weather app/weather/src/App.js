b39d0087780457e6ebedcdcdd93198d105d0818f
rachit <curiousrachit.26@gmail.com>
2023-07-19 22:04:32 +0530

    all the thing completed

--- weather app/weather/src/App.js
+++ weather app/weather/src/App.js
@@ -6,6 +6,33 @@ import Show_result from './components/Show_result';
 import weather from './Weather';
 import './Stle.css'
 function App() {
+
+
+  const input=document.getElementById('input');
+  const searchBtn=document.getElementById('search-btn');
+  searchBtn.addEventListener("click",()=>{
+      getWeatherDetail();
+  })
+   const getWeatherDetail=()=>{
+      const apiKey='6cbac7aff8c1b6b59caf5e560d50f464';
+      let countryName =input.value;
+      const apiURl=`https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=${apiKey}`;
+  
+      fetch(apiKey).then((response)=>{
+           return response.json()
+      })
+      .then((data)=>{
+  console.log(data);
+      })
+  }
+
+
+
+
+
+
+
+
   return (
     <div className='Weather_card'>
   <Header />
--- weather app/weather/src/Weather.js
+++ weather app/weather/src/Weather.js
@@ -1,18 +1,37 @@
 
-const input=document.getElementById('input');
-const searchBtn=document.getElementById('search-btn');
-searchBtn.addEventListener("click",()=>{
-    getWeatherDetail();
-})
- const getWeatherDetail=()=>{
-    const apiKey='6cbac7aff8c1b6b59caf5e560d50f464';
-    let countryName =input.value;
-    const apiURl=`https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=${apiKey}`;
+import React from 'react'
 
-    fetch(apiKey).then((response)=>{
-         return response.json()
+export default function Weather() {
+  return (
+<>
+    const input=document.getElementById('input');
+    const searchBtn=document.getElementById('search-btn');
+    searchBtn.addEventListener("click",()=>{
+        getWeatherDetail();
     })
-    .then((data)=>{
-console.log(data);
-    })
+     const getWeatherDetail=()=>{
+        const apiKey='6cbac7aff8c1b6b59caf5e560d50f464';
+        let countryName =input.value;
+        const apiURl=`https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=${apiKey}`;
+    
+        fetch(apiKey).then((response)=>{
+             return response.json()
+        })
+        .then((data)=>{
+    console.log(data);
+        })
+    }
+        
+</>
+  )
 }
+
+
+
+
+
+
+
+
+
+
