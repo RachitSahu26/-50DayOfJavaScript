65342bf29862f86e86dda29c237a5964ed0c1b0a
rachit <curiousrachit.26@gmail.com>
2023-07-23 12:04:10 +0530

    creating a weather app

--- weather app/weather/src/App.js
+++ weather app/weather/src/App.js
@@ -1,44 +1,13 @@
 import React from 'react'
-import Header from './components/Header'
-import Input_box from './components/Input_box'
-import Button from './components/Button'
-import Show_result from './components/Show_result';
-import weather from './Weather';
+
+import Weather from './Weather';
 import './Stle.css'
 function App() {
 
 
-  const input=document.getElementById('input');
-  const searchBtn=document.getElementById('search-btn');
-  searchBtn.addEventListener("click",()=>{
-      getWeatherDetail();
-  })
-   const getWeatherDetail=()=>{
-      const apiKey='6cbac7aff8c1b6b59caf5e560d50f464';
-      let countryName =input.value;
-      const apiURl=`https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=${apiKey}`;
-  
-      fetch(apiKey).then((response)=>{
-           return response.json()
-      })
-      .then((data)=>{
-  console.log(data);
-      })
-  }
-
-
-
-
-
-
-
-
   return (
     <div className='Weather_card'>
-  <Header />
-  <Input_box/>
-  <Button/>
-  <Show_result />
+<Weather/>
     </div>
   )
 }
--- weather app/weather/src/Weather.js
+++ weather app/weather/src/Weather.js
@@ -1,36 +1,74 @@
 
-import React from 'react'
+
+
+
+import React, { useEffect, useState } from 'react'
 
 export default function Weather() {
-  return (
-<>
-    const input=document.getElementById('input');
-    const searchBtn=document.getElementById('search-btn');
-    searchBtn.addEventListener("click",()=>{
-        getWeatherDetail();
-    })
-     const getWeatherDetail=()=>{
-        const apiKey='6cbac7aff8c1b6b59caf5e560d50f464';
-        let countryName =input.value;
-        const apiURl=`https://api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=${apiKey}`;
-    
-        fetch(apiKey).then((response)=>{
-             return response.json()
-        })
-        .then((data)=>{
-    console.log(data);
-        })
-    }
-        
-</>
-  )
+
+    const [city, setCity] = useState("Mumbai")
+
+    const [search, setSearch] = useState("Mumbai")
+    useEffect(() => {
+        const fetchWeather = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=6cbac7aff8c1b6b59caf5e560d50f464`
+            const response = await fetch(url);
+            const responjosn = await response.json();
+            setCity(responjosn.main);
+
+        }
+        fetchWeather();
+
+    }, [search])
+
+    return (
+        <>
+            <div className='Weather-head'>
+                <h1 > Weather App</h1>
+            </div>
+            <div className='Input-box'>
+
+                <input
+                    type='text'
+                    placeholder='Enter any city name'
+                    className='input'
+                    id='input'
+                    onChange={(event) => { setSearch(event.target.value) }}></input>
+            </div>
+
+            {
+                !city ?
+                    (<p>no data found</p>) : (
+
+                        <>
+                            <div className='show-result-box'>
+                                <h1 id='temprature'>{city.temp}</h1>
+                                <h2 id='city_name'>{search}</h2>
+                            </div>
+
+                            <div className="extra-detail">
+                                <div className="humidity">
+                                    <h1 id='Humidity-detail'> 50%</h1>
+                                    <h3 id='humi-tex'>Humidity</h3>
+                                </div>
+                                <div className="wind-speed">
+                                    <h1 id='wind-detail'> 50%</h1>
+                                    <h3 id='speed-tex'>Wind Speed</h3>
+                                </div>
+                            </div>
+
+                        </>
+
+
+
+
+                    )
+            }
+
+
+        </>
+    )
 }
-
-
-
-
-
-
 
 
 
