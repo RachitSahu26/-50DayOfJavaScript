2629989cfe76010785989ab2eca783b959b8cb04
rachit <curiousrachit.26@gmail.com>
2023-08-04 23:27:06 +0530

    added a style on button

--- weather app/weather/src/Weather.js
+++ weather app/weather/src/Weather.js
@@ -11,15 +11,25 @@ export default function Weather() {
     const [search, setSearch] = useState("Mumbai")
     useEffect(() => {
         const fetchWeather = async () => {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=6cbac7aff8c1b6b59caf5e560d50f464`
+          try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY`;
             const response = await fetch(url);
-            const responjosn = await response.json();
-            setCity(responjosn.main);
-
-        }
+            if (response.ok) {
+              const responjosn = await response.json();
+              setCity({
+                temp: responjosn.main.temp,
+                humidity: responjosn.main.humidity,
+                windSpeed: responjosn.wind.speed,
+              });
+            } else {
+              setCity(null);
+            }
+          } catch (error) {
+            setCity(null);
+          }
+        };
         fetchWeather();
-
-    }, [search])
+      }, [city]);
 
     return (
         <>
@@ -42,13 +52,14 @@ export default function Weather() {
 
                         <>
                             <div className='show-result-box'>
-                                <h1 id='temprature'>{city.temp}</h1>
+                                <h1 id='temprature'>{setCity.temp}</h1>
                                 <h2 id='city_name'>{search}</h2>
                             </div>
 
                             <div className="extra-detail">
                                 <div className="humidity">
-                                    <h1 id='Humidity-detail'> 50%</h1>
+                                    <h1 id='Humidity-detail'>{setCity.humidity}
+                                    </h1>
                                     <h3 id='humi-tex'>Humidity</h3>
                                 </div>
                                 <div className="wind-speed">
