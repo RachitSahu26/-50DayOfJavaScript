0d43705ad48af917a9e09e7730ec32c92f2f31d1
rachit <curiousrachit.26@gmail.com>
2023-08-07 23:06:21 +0530

    completed with style

--- Gallery image search/index.js
+++ Gallery image search/index.js
@@ -1,29 +1,37 @@
-const searchClick = document.getElementById("search-btn");
+const accessKey = '7IlCKWs7Wf9DKbGFpQ2zbftDTRzzfbQydwSECbfear4';
+const imagShowing = document.getElementById('image-showing');
+const searchForm = document.getElementById('search-form');
 
+const inputValue = document.getElementById("input");
+const showImage = async (userProviderValue) => {
+    const url = `https://api.unsplash.com/photos/?query=${userProviderValue}&per_page=28&client_id=${accessKey}`;
 
-const showingResult = document.querySelector('imag-box1');
-
-let keyword = '';
-let page = 1;
-async function getImage() {
-    keyword = input.value;
-    const url = `https://api.unsplash.com/search/collections?page=${page}&query=${keyword}office&client_id=7IlCKWs7Wf9DKbGFpQ2zbftDTRzzfbQydwSECbfear4`;
 
     const response = await fetch(url)
-    const data = await response.json();
-  
-    const results = data.results;
-    results.map((result) => {
-        let image = document.createElement('img')
-        image.src = result.urls.small;
-        showingResult.appendChild(image);
-    })
+    const data = await response.json()
+
+
+    data.forEach(photo => {
+        const imagebox = document.createElement('div')
+        imagebox.classList.add("img-card")
+        imagebox.innerHTML = `<img src="${photo.urls.regular}">`
+        imagShowing.appendChild(imagebox);
+    });
 
 
 }
-searchClick.addEventListener("click", (e) => {
-    e.preventDefault();
-    page = 1;
 
-    getImage()
-})
+searchForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    const userProviderValue = inputValue.value.trim();
+    if (userProviderValue !== '') {
+        showImage(userProviderValue);
+
+    } else {
+
+
+        imagShowing.innerHTML = `<h1>Please Enter a Query.</h1>`;
+
+    }
+});
